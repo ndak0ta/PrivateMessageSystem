be25ad50a491c2935968627cce53fa62c0f2bb40
ndak0ta <alihsnkhrmn@protonmail.com>
2021-10-20 18:01:08 +0300

    finishing touches before launch test

--- src/main/java/ndak0ta/privatemessagesystem/ReplyCommand.java
+++ src/main/java/ndak0ta/privatemessagesystem/ReplyCommand.java
@@ -9,28 +9,34 @@ import org.bukkit.entity.Player;
 public class ReplyCommand implements CommandExecutor {
     private Main main;
 
-    public ReplyCommand(Main main) { this.main = main; }
+    public ReplyCommand(Main main) {
+        this.main = main;
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player player = (Player) sender;
 
-        if (main.getMessageManager().recentlyMessaged.containsKey(player)) {
-            if (main.getMessageManager().recentlyMessaged.get(player) != null) {
-                Player target = main.getMessageManager().recentlyMessaged.get(player);
-                StringBuilder message = new StringBuilder();
+        if (args.length > 0) {
+            if (main.getMessageManager().recentlyMessaged.containsKey(player)) {
+                if (main.getMessageManager().recentlyMessaged.get(player) != null) {
+                    Player target = main.getMessageManager().recentlyMessaged.get(player);
+                    StringBuilder message = new StringBuilder();
 
-                for (int i = 1; i < args.length; i++) {
-                    message.append(args[i]).append(" ");
+                    for (int i = 1; i < args.length; i++) {
+                        message.append(args[i]).append(" ");
+                    }
+
+                    player.sendMessage(ChatColor.GREEN + "-> " + target.getName() + ChatColor.GRAY + " " + message.toString());
+                    target.sendMessage(ChatColor.GREEN + "<- " + player.getName() + ChatColor.GRAY + " " + message.toString());
+                } else {
+                    player.sendMessage(ChatColor.RED + "That player has logged out!");
                 }
-
-                player.sendMessage(ChatColor.GREEN + "-> " + target.getName() + ChatColor.GRAY + " " + message.toString());
-                target.sendMessage(ChatColor.GREEN + "<- " + player.getName() + ChatColor.GRAY + " " + message.toString());
             } else {
-                player.sendMessage(ChatColor.RED + "That player has logged out!");
+                player.sendMessage(ChatColor.RED + "You have not messaged anyone recently.");
             }
         } else {
-            player.sendMessage(ChatColor.RED + "You have not messaged anyone recently.");
+            player.sendMessage(ChatColor.RED + "Invalid usage! Use /message <player> <message>");
         }
 
         return false;
